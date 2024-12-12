596a8b7b05c47c9d39f87e281fb9fb2e6a11a511
Arthur Wicky <arthur.wicky@edu.univ-fcomte.fr>
2024-12-12 22:00:59 +0100

    finalisation header et menu

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+const page = document.body;
+
+if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      const isOpen = toggle.ariaExpanded === "true";
+      const isClosed = !isOpen;
+      // Mise à jour des attributs ARIA pour accessibilité
+      toggle.ariaExpanded = isClosed;
+      nav.ariaHidden = isOpen;
+      page.classList.toggle("noscroll", isClosed);
+    });
+  }
