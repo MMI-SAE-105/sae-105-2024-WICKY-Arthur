4e3145e9097fd49a0c24ed517d744a43f3f641eb
WICKY-Arthur <arthur.wicky@edu.univ-fcomte.fr>
2024-12-13 16:16:25 +0100

    favicon et page fiche de film

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,20 @@
+/*menu*/
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 const page = document.body;
 
 if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      const isOpen = toggle.ariaExpanded === "true";
-      const isClosed = !isOpen;
-      // Mise à jour des attributs ARIA pour accessibilité
-      toggle.ariaExpanded = isClosed;
-      nav.ariaHidden = isOpen;
-      page.classList.toggle("noscroll", isClosed);
-    });
-  }
+  toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    // Mise à jour des attributs ARIA pour accessibilité
+    toggle.ariaExpanded = isClosed;
+    nav.ariaHidden = isOpen;
+    page.classList.toggle("noscroll", isClosed);
+  });
+}
 
+/*carousel*/
 const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
@@ -27,4 +29,30 @@ if (carousel) {
 
   nextButton.addEventListener("click", () => {
     carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-  });}
+  });
+}
+
+/*lightbox*/
+const lightbox = document.querySelector('#lightbox');
+const listThumbnail = document.querySelectorAll("[data-full-img]");
+const lightboxImg = document.querySelector('#lightbox > img');
+
+listThumbnail.forEach((thumbnail) => {
+  thumbnail.addEventListener('click', (evt) => {
+    lightbox.classList.add('entrer');
+    lightboxImg.src = thumbnail.dataset.fullImg;
+    lightbox.showModal();
+
+  })
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.classList.add('sortie');
+  lightbox.addEventListener('animationend', () => {
+    lightbox.classList.remove('sortie');
+    lightbox.close();
+  }, { once: true });
+});
+
+
+
